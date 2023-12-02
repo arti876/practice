b85efd173ffed3c402cb7c1c83fa496d86533083
Artem <e.artem.v@gmail.com>
2023-12-02 14:38:54 +0300

    commit

--- homework/hw-todo/index.js
+++ homework/hw-todo/index.js
@@ -125,32 +125,173 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // ------------------------------------------------------------------------------
 
+  // добавляем новую карточку
+  function addNewCard() {
+    if (!document.querySelector('.header__input-text').value) {
+      inputTextHeader.classList.add('header__input-text--error');
+      inputTextHeader.addEventListener('focus', function () {
+        inputTextHeader.classList.remove('header__input-text--error');
+      });
+    } else {
+      const todoObj = getTodoObj(document.querySelector('.header__input-text').value);
+      createTodoCard(todoObj);
+      todos.push(todoObj);
+      setName(todos);
+      updateCounterCards(headerShowAllNum, 'card__item', headerShowCompletedNum, 'card__item--checked');
+    };
+  }
+
+  // стилизуем карточку с помощью checkbox
+  function pressСheckbox() {
+    event.target.closest('.card__btn--confirm').classList.toggle('card__btn--confirm-checked');
+    event.target.closest('.card__item').classList.toggle('card__item--checked');
+    event.target.closest('.card__item').querySelector('.card__todo-text').classList.toggle('card__todo-text--del');
+    // обновление счетчика карточек
+    updateCounterCards(headerShowAllNum, 'card__item', headerShowCompletedNum, 'card__item--checked');
+    // проверяем значение isChecked и обновляем localStorage
+    for (let i = 0; i < todos.length; i++) {
+      if (todos[i].id === event.target.closest('.card__item').id) {
+        todos[i].isChecked = !todos[i].isChecked;
+        setName(todos);
+      };
+    };
+  }
+
+  // удаляем карточку при нажатии на крестик
+  function crossCardRemoval() {
+    event.target.closest('.card__item').remove();
+    // обновляем localStorage
+    const cardDel = todos.filter(todo => todo.id === event.target.closest('.card__item').id);
+    todos.splice(cardDel, 1);
+    setName(todos);
+    // обновление счетчика карточек
+    updateCounterCards(headerShowAllNum, 'card__item', headerShowCompletedNum, 'card__item--checked');
+  }
+
+  // удаление всех карточек
+  function delAllCards() {
+    const cardInputAll = document.querySelectorAll('.card__item');
+    cardInputAll.forEach(el => el.remove());
+    // обновление счетчика карточек
+    updateCounterCards(headerShowAllNum, 'card__item', headerShowCompletedNum, 'card__item--checked');
+    // очищаем localStorage
+    todos = [];
+    setName(todos);
+  }
+
+  // показать все скрытые карточки
+  function showAllCards() {
+    document.querySelectorAll('.card__item').forEach(el => el.classList.remove('hidden'));
+  }
+
+  // показать только выбранные карточки
+  function showCompletedCards() {
+    document.querySelectorAll('.card__item').forEach(el => el.classList.contains('card__item--checked') ? el : el.classList.add('hidden'));
+  }
+
+  // удаляем последнюю карточку в списке
+  function delLastCard() {
+    const cardDelLast = document.querySelector('.card__list');
+    cardDelLast.lastChild.remove();
+    // обновляем localStorage
+    todos.pop();
+    setName(todos);
+    // обновление счетчика карточек
+    updateCounterCards(headerShowAllNum, 'card__item', headerShowCompletedNum, 'card__item--checked');
+  }
+
+  function cardSearch() {
+    const todoTextCardAll = document.querySelectorAll('.card__todo-text');
+    for (let i = 0; i < todoTextCardAll.length; i++) {
+      if (todoTextCardAll[i].innerText.toLowerCase().includes(document.querySelector('.header__input-search').value.toLowerCase())) {
+        document.querySelectorAll('.card__item')[i].classList.remove('hidden');
+      } else {
+        document.querySelectorAll('.card__item')[i].classList.add('hidden');
+      };
+    };
+  };
+
+  // ------------------------------------------------------------------------------
+
   // события по 'click'
   root.addEventListener('click', function (event) {
 
-    const paramsUpdateCounterCards = {
-      allCards: document.querySelector('.header__show-all-num'),
-      allCardsсlass: 'card__item',
-      completedCard: document.querySelector('.header__show-complete-num'),
-      completedCardсlass: 'card__item--checked',
-    }
-
     // добавляем новую карточку
     if (event.target.classList.contains('header__btn--add')) {
-
-      const paramsAddNewCard = {
-        textValue: document.querySelector('.header__input-text').value,
-        classListAdd: 'header__input-text--error',
-        classListRemove: 'header__input-text--error',
-      }
-
-      addNewCard(paramsAddNewCard, paramsUpdateCounterCards);
+      addNewCard();
     };
 
+    // стилизуем карточку с помощью checkbox
+    if (event.target.classList.contains('card__btn--confirm')) {
+      pressСheckbox();
+    };
+
+    // удаляем карточку при нажатии на крестик
+    if (event.target.classList.contains('card__btn--cancel')) {
+      crossCardRemoval();
+    };
+
+    // удаление всех карточек
+    if (event.target.classList.contains('header__btn--del-all')) {
+      delAllCards();
+    };
+
+    // показать все скрытые карточки
+    if (event.target.classList.contains('header__btn--show-all')) {
+      showAllCards();
+    };
+
+    // показать только выбранные карточки
+    if (event.target.classList.contains('header__btn--show-completed')) {
+      showCompletedCards();
+    };
+
+    // удаляем последнюю карточку в списке
+    if (event.target.classList.contains('header__btn--del-last')) {
+      delLastCard()
+    };
   });
 
-  // // события по 'input'
-  // root.addEventListener('input', function (event) {
-  // });
+  // события по 'input'
+  root.addEventListener('input', function (event) {
+
+    // поиск карточек
+    if (event.target.classList.contains('header__input-search')) {
+      cardSearch()
+    }
+  });
 
 }, { once: true });
+
+
+
+
+
+
+
+
+// // ------------------------------------------------------------------------------
+
+// // события по 'click'
+// root.addEventListener('click', function (event) {
+
+//   const paramsUpdateCounterCards = {
+//     allCards: document.querySelector('.header__show-all-num'),
+//     allCardsсlass: 'card__item',
+//     completedCard: document.querySelector('.header__show-complete-num'),
+//     completedCardсlass: 'card__item--checked',
+//   }
+
+//   // добавляем новую карточку
+//   if (event.target.classList.contains('header__btn--add')) {
+
+//     const paramsAddNewCard = {
+//       textValue: document.querySelector('.header__input-text').value,
+//       classListAdd: 'header__input-text--error',
+//       classListRemove: 'header__input-text--error',
+//     }
+
+//     addNewCard(paramsAddNewCard, paramsUpdateCounterCards);
+//   };
+
+// });
