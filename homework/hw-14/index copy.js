1d81df45b1d162f85299a3038081661eb3c37032
arti876 <e.artem.v@gmail.com>
2023-12-05 17:23:39 +0300

    commit

--- homework/hw-14/index copy.js
+++ homework/hw-14/index copy.js
@@ -7,12 +7,13 @@ const btnReset = document.querySelector('.reset');
 // -----------------------------------------------------------
 
 // function Constructor(num) {
-//   let x = this
+  // let x = this
 //   this.num = num;
 
 //   this.go = function() {
 
 //       setInterval(function () {
+          
 //           x.num++;
 //       }, 100);
 //   };
@@ -64,67 +65,3 @@ wrapper.addEventListener('click', function (event) {
 });
 
 // -----------------------------------------------------------
-
-//   // получить
-//   function getName() {
-//     return JSON.parse(localStorage.getItem('num')) ?? +0;
-//   };
-
-//   // записать
-//   function setName(num) {
-//     localStorage.setItem('num', JSON.stringify(num));
-//   };
-
-//   let num = getName();
-
-// function Timer(fn, t) {
-//   // let methodSetIntervalTimer;
-//   timer.textContent = num;
-//   var timerObj = setInterval(fn, t);
-
-//   this.stop = function () {
-//     if (timerObj) {
-//       clearInterval(timerObj);
-//       timerObj = null;
-//     };
-//     return this;
-//   }
-
-//   this.start = function () {
-//     if (!timerObj) {
-//       timerObj = setInterval(startTimer(getName()), 300);
-//     }
-//     return this;
-//   }
-
-//   this.reset = function () {
-//         clearInterval(timerObj);
-//         timerObj = null;
-//         timer.textContent = 0;
-//         setName(+timer.textContent)
-//     return this;
-//   }
-// }
-
-// function startTimer(num) {
-//   timer.textContent = num++
-//   setName(num - 1);
-// }
-
-
-// var timer1 = new Timer(startTimer(num), 300);
-
-// wrapper.addEventListener('click', function (event) {
-//   if (event.target.classList.contains('start')) {
-//     timer1.start();
-//     console.log(timer1.start())
-//   };
-
-//   if (event.target.classList.contains('pause')) {
-//     timer1.stop();
-//   };
-
-//   if (event.target.classList.contains('reset')) {
-//     timer1.reset()
-//   };
-// });
