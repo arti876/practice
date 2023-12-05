19a20a4bf4b219edb9532ebf175922add75bbe47
Artem <e.artem.v@gmail.com>
2023-12-05 22:15:30 +0300

    commit

--- homework/hw-14/index copy.js
+++ homework/hw-14/index copy.js
@@ -7,13 +7,12 @@ const btnReset = document.querySelector('.reset');
 // -----------------------------------------------------------
 
 // function Constructor(num) {
-  // let x = this
+//   let x = this
 //   this.num = num;
 
 //   this.go = function() {
 
 //       setInterval(function () {
-          
 //           x.num++;
 //       }, 100);
 //   };
@@ -21,9 +20,54 @@ const btnReset = document.querySelector('.reset');
 
 // -----------------------------------------------------------
 
+// // получить
+// function getName() {
+//   return JSON.parse(localStorage.getItem('num')) ?? 0;
+// };
+
+// // записать
+// function setName(num) {
+//   localStorage.setItem('num', JSON.stringify(num));
+// };
+
+// let num = getName();
+// let methodSetIntervalTimer;
+// timer.textContent = num;
+
+// const startTimer = (num) => {
+//   methodSetIntervalTimer = setInterval(() => {
+//     timer.textContent = num++;
+//     setName(num - 1);
+//   }, 300);
+// };
+
+// wrapper.addEventListener('click', function (event) {
+//   if (event.target.classList.contains('start')) {
+//     if (!methodSetIntervalTimer) {
+//       startTimer(getName());
+//     };
+//   };
+
+//   if (event.target.classList.contains('pause')) {
+//     if (methodSetIntervalTimer) {
+//       clearInterval(methodSetIntervalTimer);
+//       methodSetIntervalTimer = null;
+//     };
+//   };
+
+//   if (event.target.classList.contains('reset')) {
+//     clearInterval(methodSetIntervalTimer);
+//     methodSetIntervalTimer = null;
+//     timer.textContent = 0;
+//     setName(+timer.textContent)
+//   };
+// });
+
+// -----------------------------------------------------------
+
 // получить
 function getName() {
-  return JSON.parse(localStorage.getItem('num')) ?? +0;
+  return JSON.parse(localStorage.getItem('num')) ?? 0;
 };
 
 // записать
@@ -31,37 +75,53 @@ function setName(num) {
   localStorage.setItem('num', JSON.stringify(num));
 };
 
-let num = getName();
-let methodSetIntervalTimer;
-timer.textContent = num;
+function Timer(num) {
+  let methodSetIntervalTimer;
 
-const startTimer = (num) => {
-  methodSetIntervalTimer = setInterval(() => {
-    timer.textContent = num++
-    setName(num - 1);
-  }, 300);
-}
+  const startTimer = (num) => {
+    methodSetIntervalTimer = setInterval(() => {
+      timer.textContent = num++;
+      setName(num - 1);
+    }, 1000);
+  };
 
-wrapper.addEventListener('click', function (event) {
-  if (event.target.classList.contains('start')) {
+  this.start = function () {
     if (!methodSetIntervalTimer) {
       startTimer(getName());
     };
   };
 
-  if (event.target.classList.contains('pause')) {
+  this.stop = function () {
     if (methodSetIntervalTimer) {
       clearInterval(methodSetIntervalTimer);
       methodSetIntervalTimer = null;
     };
   };
 
-  if (event.target.classList.contains('reset')) {
+  this.reset = function () {
     clearInterval(methodSetIntervalTimer);
     methodSetIntervalTimer = null;
     timer.textContent = 0;
-    setName(+timer.textContent)
+    setName(+timer.textContent);
+  };
+}
+
+let num = getName();
+timer.textContent = num;
+
+let newTimer = new Timer(timer.textContent);
+
+wrapper.addEventListener('click', function (event) {
+
+  if (event.target.classList.contains('start')) {
+    newTimer.start();
+  };
+
+  if (event.target.classList.contains('pause')) {
+    newTimer.stop();
+  };
+
+  if (event.target.classList.contains('reset')) {
+    newTimer.reset();
   };
 });
-
-// -----------------------------------------------------------
--- homework/hw-14/index.js
+++ homework/hw-14/index.js
@@ -4,78 +4,27 @@ const btnStart = document.querySelector('.start');
 const btnPause = document.querySelector('.pause');
 const btnReset = document.querySelector('.reset');
 
-// -----------------------------------------------------------
+function getName() {
+  return JSON.parse(localStorage.getItem('num')) ?? 0;
+};
 
-// function Constructor(num) {
-//   let x = this
-//   this.num = num;
+function setName(num) {
+  localStorage.setItem('num', JSON.stringify(num));
+};
 
-//   this.go = function() {
-
-//       setInterval(function () {
-//           x.num++;
-//       }, 100);
-//   };
-// }
-
-// -----------------------------------------------------------
-
-// // получить
-// function getName() {
-//   return JSON.parse(localStorage.getItem('num')) ?? +0;
-// };
-
-// // записать
-// function setName(num) {
-//   localStorage.setItem('num', JSON.stringify(num));
-// };
-
-// let num = getName();
-// let methodSetIntervalTimer;
-// timer.textContent = num;
-
-// const startTimer = (num) => {
-//   methodSetIntervalTimer = setInterval(() => {
-//     timer.textContent = num++
-//     setName(num - 1);
-//   }, 300);
-// }
-
-// wrapper.addEventListener('click', function (event) {
-//   if (event.target.classList.contains('start')) {
-//     if (!methodSetIntervalTimer) {
-//       startTimer(getName());
-//     };
-//   };
-
-//   if (event.target.classList.contains('pause')) {
-//     if (methodSetIntervalTimer) {
-//       clearInterval(methodSetIntervalTimer);
-//       methodSetIntervalTimer = null;
-//     };
-//   };
-
-//   if (event.target.classList.contains('reset')) {
-//     clearInterval(methodSetIntervalTimer);
-//     methodSetIntervalTimer = null;
-//     timer.textContent = 0;
-//     setName(+timer.textContent)
-//   };
-// });
-
-// -----------------------------------------------------------
-
-function Clock(num) {
+function Timer(num) {
   let methodSetIntervalTimer;
 
-  function render() {
-    timer.textContent = num++
-    setName(num - 1);
-  }
+  const startTimer = (num) => {
+    methodSetIntervalTimer = setInterval(() => {
+      timer.textContent = num++;
+      setName(num - 1);
+    }, 1000);
+  };
 
   this.start = function () {
     if (!methodSetIntervalTimer) {
-      methodSetIntervalTimer = setInterval(render, 300);
+      startTimer(getName());
     };
   };
 
@@ -89,39 +38,26 @@ function Clock(num) {
   this.reset = function () {
     clearInterval(methodSetIntervalTimer);
     methodSetIntervalTimer = null;
-    // timer.textContent = 0;
-    // setName(+timer.textContent)
+    timer.textContent = 0;
+    setName(+timer.textContent);
   };
 }
 
-// получить
-function getName() {
-  return JSON.parse(localStorage.getItem('num')) ?? 0;
-};
+let num = getName();
+timer.textContent = num;
 
-// записать
-function setName(num) {
-  localStorage.setItem('num', JSON.stringify(num));
-};
-
-// let num = getName();
-// timer.textContent = num;
-
-timer.textContent = getName();
-
-let clock = new Clock(timer.textContent);
+let newTimer = new Timer(timer.textContent);
 
 wrapper.addEventListener('click', function (event) {
-
   if (event.target.classList.contains('start')) {
-    clock.start();
+    newTimer.start();
   };
 
   if (event.target.classList.contains('pause')) {
-    clock.stop();
+    newTimer.stop();
   };
 
   if (event.target.classList.contains('reset')) {
-    clock.reset();
+    newTimer.reset();
   };
 });
