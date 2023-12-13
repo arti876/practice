b070aad2785642e6832e962f6b03274d29132f2e
arti876 <e.artem.v@gmail.com>
2023-12-13 13:24:10 +0300

    commit

--- homework/hw-16/index.js
+++ homework/hw-16/index.js
@@ -15,8 +15,8 @@
 
 const root = document.querySelector('.root');
 
-function printTodos({ userId, id, title, body }) {
-  if(!userId || !id || !title || !body) {
+function printPots({ userId, id, title, body }) {
+  if (!userId || !id || !title || !body) {
     return
   } else {
     const postItem = document.createElement("div");
@@ -33,19 +33,50 @@ function printTodos({ userId, id, title, body }) {
 };
 
 function fetchPosts(idArr) {
-return idArr.map(id => fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-  .then(response => response.json()))
+  return idArr.map(id => fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    .then(response => response.json()))
 }
 
 function renderPosts(...idPosts) {
-  Promise.allSettled(fetchPosts(idPosts.flat(1)))
-    .then(response => response.forEach(el => printTodos(el.value)))
+  const idArr = idPosts.flat(1);
+
+  if (idArr.some((id) => id === true)) {
+    fetch(`https://jsonplaceholder.typicode.com/posts/1`)
+    .then(response => response.json())
+    .then(response => printPots(response))
+  } else {
+    Promise.allSettled(fetchPosts(idArr))
+    .then(response => response.forEach(el => printPots(el.value)))
+    .catch((e) => alert(e))
+  }
 };
 
-renderPosts(['sdfsdf', 23, 7, 3])
+renderPosts([true, 7, 3])
 // renderPosts(1,2,3,4,5,6)
 // renderPosts(1,2)
 // renderPosts([1,2,3,4,5,6])
 // renderPosts([1,2])
 // renderPosts({})
 // renderPosts(true, false)
+
+//------------------------------------------------
+
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  console.log(h + ":" + m + ":" + s)
+  setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+  if (i < 10) { i = "0" + i };
+  return i;
+}
+
+startTime()
+
+//------------------------------------------------
