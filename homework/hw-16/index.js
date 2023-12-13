9b11499531559e6602b47cb81400ddbf0d6e55d0
arti876 <e.artem.v@gmail.com>
2023-12-13 12:53:17 +0300

    work.v1

--- homework/hw-16/index.js
+++ homework/hw-16/index.js
@@ -1,42 +1,4 @@
-// const root = document.querySelector('.root');
-// const URL = 'https://jsonplaceholder.typicode.com/posts/';
-
-// function getTodos() {
-//   fetch(URL)
-//     .then(response => response.json())
-//     .then((data) => printTodos(data));
-// };
-
-// function printTodos(todos) {
-//   todos.forEach(({ userId, id, title, body }) => {
-//     const postItem = document.createElement("div");
-//     postItem.classList.add('post-item');
-//     root.append(postItem);
-//     const postTitle = document.createElement("div");
-//     postTitle.classList.add('post-title');
-//     postTitle.textContent = `${id} : ${title}`;
-//     const postBody = document.createElement("div");
-//     postBody.classList.add('post-body');
-//     postBody.textContent = `${userId} : ${body}`;
-//     postItem.append(postTitle, postBody);
-//   });
-// };
-
-// getTodos();
-
-// function fetchPosts (posts) {
-// let requests = posts.map(urlPost => fetch(`https://jsonplaceholder.typicode.com/posts/${urlPost}`));
-// console.log(requests)
-// }
-
-// fetchPosts([1,2,3,4,5,6]);
-
-// --------------------------------------------------------------------
-// --------------------------------------------------------------------
-// --------------------------------------------------------------------
-
-
-// 'https://jsonplaceholder.typicode.com/posts/'
+// 'https://jsonplaceholder.typicode.com/idPosts/'
 
 // Сначала 15, потом 23, потом 7 и только потом 3
 
@@ -51,36 +13,12 @@
 // --------------------------------------------------------------------
 // --------------------------------------------------------------------
 
-// function getTodos() {
-//   return fetch('https://jsonplaceholder.typicode.com/posts/17')
-//     .then(response => response.json())
-// }
-
-// async function get() {
-//   const [todosResult] = await Promise.allSettled([getTodos()])
-
-//   if (todosResult.status === 'rejected') {
-//     console.error('Error getting todos:', todosResult.reason)
-//   } else {
-//     console.log(todosResult.value)
-//   }
-// }
-
-// get()
-
 const root = document.querySelector('.root');
 
-function fetchPosts (posts) {
-return posts.map(urlPost => fetch(`https://jsonplaceholder.typicode.com/posts/${urlPost}`)
-.then(response => response.json())
-.then(response => console.log(response))
-// .then(response => printTodos(response))
-)};
-
-fetchPosts ([15,23,7,3])
-
-  function printTodos(post) {
-  let { userId, id, title, body } = post
+function printTodos({ userId, id, title, body }) {
+  if(!userId || !id || !title || !body) {
+    return
+  } else {
     const postItem = document.createElement("div");
     postItem.classList.add('post-item');
     root.append(postItem);
@@ -91,4 +29,23 @@ fetchPosts ([15,23,7,3])
     postBody.classList.add('post-body');
     postBody.textContent = `${userId} : ${body}`;
     postItem.append(postTitle, postBody);
+  }
 };
+
+function fetchPosts(idArr) {
+return idArr.map(id => fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+  .then(response => response.json()))
+}
+
+function renderPosts(...idPosts) {
+  Promise.allSettled(fetchPosts(idPosts.flat(1)))
+    .then(response => response.forEach(el => printTodos(el.value)))
+};
+
+renderPosts(['sdfsdf', 23, 7, 3])
+// renderPosts(1,2,3,4,5,6)
+// renderPosts(1,2)
+// renderPosts([1,2,3,4,5,6])
+// renderPosts([1,2])
+// renderPosts({})
+// renderPosts(true, false)
