6181901d1c3d12ed5491bbd5fef00a6d4d30a4a9
Artem <e.artem.v@gmail.com>
2023-12-14 22:46:16 +0300

    commit

--- active/index.js
+++ active/index.js
@@ -21,29 +21,29 @@
 //   return textNode
 // }
 
-function renderElement(template) {
-  document.body.insertAdjacentHTML('beforeend', template)
-}
+// function renderElement(template) {
+//   document.body.insertAdjacentHTML('beforeend', template)
+// }
 
-function createWidget() {
-  return new Promise ((resolve, reject) => {
-    const templateWidget = '<fxwidget-cc amount="100" decimals="2" large="false" shadow="true" symbol="true" grouping="true" bor
-    const scriptUrl = 'https://s.fx-w.io/widgets/currency-converter/latest.js?ru'
-    const scriptElement = document.createElement('script')
+// function createWidget() {
+//   return new Promise ((resolve, reject) => {
+//     const templateWidget = '<fxwidget-cc amount="100" decimals="2" large="false" shadow="true" symbol="true" grouping="true" bor
+//     const scriptUrl = 'https://s.fx-w.io/widgets/currency-converter/latest.js?ru'
+//     const scriptElement = document.createElement('script')
   
-    scriptElement.src = scriptUrl
+//     scriptElement.src = scriptUrl
   
-    scriptElement.onload = () => {
-      resolve(templateWidget)
-      console.log('Widget is loaded!')
-    }
+//     scriptElement.onload = () => {
+//       resolve(templateWidget)
+//       console.log('Widget is loaded!')
+//     }
   
-    scriptElement.onerror = () => {
-      reject(new Error('Error! Try again later'))
-    }
+//     scriptElement.onerror = () => {
+//       reject(new Error('Error! Try again later'))
+//     }
   
-    document.head.append(scriptElement)
-  })
-}
+//     document.head.append(scriptElement)
+//   })
+// }
 
-createWidget().then((result) => renderElement(result)).catch((error) => console.error(error))
+// createWidget().then((result) => renderElement(result)).catch((error) => console.error(error))
